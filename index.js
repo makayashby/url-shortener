abd03e1a29fdf2a2195182d30cbc876dfafa8e43
Makay Ashby <makayashby33@gmail.com>
2023-10-04 16:47:28 -0600

    first commit

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let arr = [];
+let num = 0;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.urlencoded({ extended: false }))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -19,6 +21,30 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/shorturl/:urlID', (req, res) => {
+  const urlID = req.params.urlID
+  const foundURL = arr.find(({ id }) => id === Number(urlID))
+  if (!foundURL) {
+    res.status(400).send('please provide a valid shorturl id')
+  }
+  res.status(301).redirect(foundURL.url)
+  // res.status(200).json(foundURL.url)
+})
+
+app.post('/api/shorturl', (req, res) => {
+  if (!req.body.url.startsWith("http")) {
+    res.json({ error: "invalid url" })
+  }
+  arr = [...arr, { "id": num, "url": req.body.url }]
+  res.json({
+    original_url: arr[num].url,
+    short_url: arr[num].id
+  })
+  console.log(arr)
+  num++
+})
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
